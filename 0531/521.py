8ac397f041dfaa91fc166845e5ee4cba3ff8fecc
Zhi Li <lizhicq@users.noreply.github.com>
2018-05-31 19:56:49 -0400

    update

---
+++ 0531/521.py
@@ -0,0 +1,7 @@
+class Solution:
+    """
+    @param: nums: an array of integers
+    @return: the number of unique integers
+    """
+    def deduplication(self, nums):
+        # write your code here
