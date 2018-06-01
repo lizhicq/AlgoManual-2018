b77e3d44db48334d9d4001e271d855a0d720a2a2
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-01 14:27:05 -0400

    update

--- 0601/464. Sort Integers II.py
+++ 0601/464. Sort Integers II.py
@@ -7,14 +7,23 @@ class Solution:
         pass
 
     def quicksort(self, nums):
-
+        pass
 
     def partition(self, nums, lo, hi):
-        pivot = nums[hi]
-
-
+        pvt = lo
+        for i in range(lo, hi):
+            if nums[i] < nums[hi]:
+                nums[i], nums[pvt] = nums[pvt], nums[i]
+                pvt += 1
+        nums[pvt], nums[hi] = nums[hi], nums[pvt]
+        print nums
+        return pvt
 
     def mergesort(self, nums):
         pass
     def heapsort(self, nums):
         pass
+
+
+if __name__ == "__main__":
+    print Solution().partition([1,2,3,4,5,6,7,5.5], 0, 7)
