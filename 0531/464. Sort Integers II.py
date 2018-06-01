c337edbc926c1f9334d979dee2b8a680ad8c469c
Zhi Li <lizhicq@users.noreply.github.com>
2018-05-31 21:05:26 -0400

    update

---
+++ 0531/464. Sort Integers II.py
@@ -0,0 +1,13 @@
+class Solution:
+    """
+    @param A: an integer array
+    @return: nothing
+    """
+    def sortIntegers2(self, A):
+        pass
+
+    def quicksort(self, nums):
+
+    def mergesort(self, nums):
+
+    def heapsort(self, nums):
--- 0531/521.py
+++ 0531/521.py
@@ -4,4 +4,14 @@ class Solution:
     @return: the number of unique integers
     """
     def deduplication(self, nums):
-        # write your code here
+        nums = list(set(nums))
+        return len(nums)
+
+    def deduplication2(self, nums):
+        nums.sort()
+        pvt = 0
+        for i in range(len(nums)):
+            if nums[i] != nums[pvt]:
+                pvt += 1
+                nums[pvt] = nums[i]
+        return pvt + 1
