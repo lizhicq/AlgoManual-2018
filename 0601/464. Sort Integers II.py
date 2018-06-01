1d51e0d037373c0c04cccb71536f16dabf46a3b5
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-01 14:54:24 -0400

    0601, sort algorithms

--- 0601/464. Sort Integers II.py
+++ 0601/464. Sort Integers II.py
@@ -13,8 +13,21 @@ class Solution:
             self.quicksort(nums, lo, p-1)
             self.quicksort(nums, p+1, hi)
 
+    def quickselect(self, nums, k):
+        p = self.partition(nums, 0, len(nums)-1)
+        while True:
+            print p, nums
+            if p == k:
+                return nums[p]
+            elif p < k:
+                p = self.partition(nums, p+1, len(nums)-1)
+            else: #p > k
+                p = self.partition(nums, 0, p)
+
     def partition(self, nums, lo, hi):
         pvt = lo
+        mid = (lo + hi) // 2
+        nums[mid], nums[hi] = nums[hi], nums[mid]
         for i in range(lo, hi):
             if nums[i] < nums[hi]:
                 nums[i], nums[pvt] = nums[pvt], nums[i]
@@ -29,4 +42,8 @@ class Solution:
 
 
 if __name__ == "__main__":
-    print Solution().partition([1,2,3,4,5,6,7,5.5], 0, 7)
+    #print Solution().partition([1,2,3,4,5,6,7,5.5], 0, 7)
+    nums = range(10)
+    print nums
+    for i in range(10):
+        print Solution().quickselect(nums, i)
