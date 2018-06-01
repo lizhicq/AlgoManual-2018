71af6edda0d1e4058e4f19c4c4068348f9e68274
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-01 17:02:35 -0400

    0601, sort algorithms

--- 0601/464. Sort Integers II.py
+++ 0601/464. Sort Integers II.py
@@ -4,8 +4,11 @@ class Solution:
     @return: nothing
     """
     def sortIntegers2(self, A): # A.sort() Your submission beats 98.00% Submissions!
+        A.sort()
         lo, hi = 0, len(A) - 1
-        self.quicksort(A, lo, hi)
+        A = self.mergesort(A,lo,hi)
+        return A
+
 
     def quicksort(self, nums, lo, hi):# Your submission beats 67.20% Submissions!
         if lo < hi:
@@ -13,16 +16,17 @@ class Solution:
             self.quicksort(nums, lo, p-1)
             self.quicksort(nums, p+1, hi)
 
-    def quickselect(self, nums, k):
-        p = self.partition(nums, 0, len(nums)-1)
+    def quickselect(self, nums, lo, hi, k):
         while True:
-            print p, nums
-            if p == k:
-                return nums[p]
-            elif p < k:
-                p = self.partition(nums, p+1, len(nums)-1)
-            else: #p > k
-                p = self.partition(nums, 0, p)
+            if lo == hi:
+                return nums[lo]
+            pvt = self.partition(nums, lo, hi)
+            if k == pvt:
+                return nums[k]
+            elif k < pvt:
+                hi = pvt - 1
+            else:
+                lo = pvt + 1
 
     def partition(self, nums, lo, hi):
         pvt = lo
@@ -35,15 +39,51 @@ class Solution:
         nums[pvt], nums[hi] = nums[hi], nums[pvt]
         return pvt
 
-    def mergesort(self, nums):
-        pass
+    def mergesort(self, nums, lo, hi): # Your submission beats 33.00% Submissions!
+        if lo == hi:
+            return [nums[lo]]
+        mid = (lo + hi) // 2
+        left = self.mergesort(nums, lo, mid)
+        right = self.mergesort(nums, mid+1, hi)
+        return self.merge(left, right)
+
+    def merge(self, left, right): # merge two sorted list
+        res = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+        while i < len(left):
+            res.append(left[i])
+            i += 1
+        while j < len(right):
+            res.append(right[j])
+            j += 1
+        return res
+
     def heapsort(self, nums):
         pass
 
 
+
+    def heapify(self, nums, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < n and nums[i] < nums[left]:
+            largest = left
+        if right < n and nums[largest] < nums[right]:
+            largest = right
+        nums[i], nums[largest] = nums[largest], nums[i]
+        self.heapify(nums, n, largest)
+
+
+from heapq import heapify
 if __name__ == "__main__":
-    #print Solution().partition([1,2,3,4,5,6,7,5.5], 0, 7)
-    nums = range(10)
+    nums = [3,2,1,4,5]
+    heapify(nums)
     print nums
-    for i in range(10):
-        print Solution().quickselect(nums, i)
