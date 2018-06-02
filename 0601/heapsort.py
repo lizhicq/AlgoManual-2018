7c2605d597d50b0da3a44add06574f6e1a2e1cc4
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-02 10:33:19 -0400

    0601, sort algorithms

--- 0601/heapsort.py
+++ 0601/heapsort.py
@@ -3,6 +3,15 @@ class Solution:
     @param A: an integer array
     @return: nothing
     """
+    def heapsort(self, nums, lo=None, hi=None):
+        lo = 0 if lo is None else lo
+        hi = len(nums) - 1 if hi is None else hi
+
+        # 1. build the heap
+        for i in range(hi//2, -1, -1):
+            self.maxheapify(nums, i, hi)
+
+        for i in range(hi, 0, -1):
 
     def maxheapify(self, nums, lo=None, hi=None):
         lo = 0 if lo is None else lo
@@ -29,5 +38,11 @@ class Solution:
 
 if __name__ == "__main__":
     nums = [1,5,4,2,1,3,2,1,4,2,12,3,2,2,2]
-    Solution().maxheapify(nums)
+    # build maxheap
+    s = Solution()
+    for i in range(len(nums)//2 - 1, -1, -1):
+        Solution().maxheapify(nums, i, len(nums) - 1)
+    print nums
+    for i in range(len(nums)//2, -1, -1):
+        Solution().maxheapify(nums, i, len(nums) - 1)
     print nums
