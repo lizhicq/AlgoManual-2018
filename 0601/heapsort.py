5b07a4aac5b31f01ec6e396e079bd09dafc7a9a2
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-01 21:07:33 -0400

    0601, sort algorithms

--- 0601/heapsort.py
+++ 0601/heapsort.py
@@ -3,23 +3,31 @@ class Solution:
     @param A: an integer array
     @return: nothing
     """
-    def sortIntegers2(self, A): # A.sort() Your submission beats 98.00% Submissions!
-        A.sort()
-        lo, hi = 0, len(A) - 1
-        A = self.mergesort(A,lo,hi)
-        return A
 
-    def heapsort(self, nums):
-        pass
+    def maxheapify(self, nums, lo=None, hi=None):
+        lo = 0 if lo is None else lo
+        hi = len(nums) - 1 if hi is None else hi
+        parent = lo
+        while parent <= hi:
+            left = parent * 2 + 1
+            right = parent * 2 + 2
+            child = -1
+
+            if left <= hi and right <= hi:
+                child = left if nums[left] > nums[right] else right
+            elif left <= hi:
+                child = left
+            else:
+                return
+            # max heap root >= left and root >= right
+            if nums[parent] >= nums[child]:
+                return
+            nums[parent], nums[child] = nums[child], nums[parent]
+            parent = child
 
 
-    def heapify(self, nums, n, i):
-        largest = i
-        left = 2 * i + 1
-        right = 2 * i + 2
-        if left < n and nums[i] < nums[left]:
-            largest = left
-        if right < n and nums[largest] < nums[right]:
-            largest = right
-        nums[i], nums[largest] = nums[largest], nums[i]
-        self.heapify(nums, n, largest)
+
+if __name__ == "__main__":
+    nums = [1,5,4,2,1,3,2,1,4,2,12,3,2,2,2]
+    Solution().maxheapify(nums)
+    print nums
