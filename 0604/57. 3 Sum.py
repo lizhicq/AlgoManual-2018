9bae405c43b5aea7ddd942f8c062fe9dbac8b706
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-04 21:13:26 -0400

    0604 update

--- 0604/57. 3 Sum.py
+++ 0604/57. 3 Sum.py
@@ -9,16 +9,18 @@ class Solution:
         self.dfs([], 0, res, numbers)
         return res
 
-    def dfs(self, tmp, start_index, res, numbers):
-        print tmp, start_index
-        if len(tmp) == 3 or start_index == len(numbers) - 1:
+    def dfs(self, tmp, start_index, res, S):
+        #print tmp, start_index
+        if len(tmp) == 3:
             if sum(tmp) == 0:
                 #print tmp, start_index
                 res.append(tuple(tmp))
-            print 'return'
+            #print 'return'
             return
-        for i in range(start_index, len(numbers)):
-            self.dfs(tmp + [numbers[i]], start_index+1, res, numbers)
+        for i in range(start_index, len(S)):
+            if i == start_index or S[i-1] != S[i]: # duplicate or not ?
+                self.dfs(tmp + [S[i]], i+1, res, S)
 
 if __name__ == "__main__":
-    print Solution().threeSum([-4, 1, 2])
+    nums = [-11, -7, -5, -4, -4, -3, -3, -2, -2, -1, -1, 1, 2, 3, 4, 5, 5, 7]
+    print Solution().threeSum(nums)
