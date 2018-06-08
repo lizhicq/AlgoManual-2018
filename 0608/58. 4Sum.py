d76b10e9ef761d0fbf40f4d58792ca8e8a51972c
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-08 18:21:24 -0400

    0608

--- 0608/58. 4Sum.py
+++ 0608/58. 4Sum.py
@@ -37,6 +37,33 @@ class Solution:
         dfs([], 0)
         return res
 
+    def fourSum3(self, nums, target):
+        nums.sort()
+        res = []
+        length = len(nums)
+        for i in range(0, length - 3):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, length - 2):
+                if j != i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                sum = target - nums[i] - nums[j]
+                left, right = j + 1, length - 1
+                while left < right:
+                    if nums[left] + nums[right] == sum:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        right -= 1
+                        left += 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif nums[left] + nums[right] > sum:
+                        right -= 1
+                    else:
+                        left += 1
+        return res
+
 if __name__ =="__main__":
     S = [1, 0, -1, 0, -2, 2]
     target = 0
@@ -45,8 +72,11 @@ if __name__ =="__main__":
     count = 0
     for i in range(100):
         a = timeit("Solution().fourSum1(S, target)",
-                 setup="from __main__ import Solution, S, target", number=100)
+                 setup="from __main__ import Solution, S, target", number=1000)
         b = timeit("Solution().fourSum2(S, target)",
-                 setup="from __main__ import Solution, S, target", number=100)
-        count += 1 if a < b else 0
+                 setup="from __main__ import Solution, S, target", number=1000)
+        c = timeit("Solution().fourSum3(S, target)",
+                 setup="from __main__ import Solution, S, target", number=1000)
+
+        print a, b, c
     print count
