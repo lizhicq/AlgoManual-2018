cad58f1824683a8fab1d1b75f4f1ae749b351a7b
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-08 17:00:15 -0400

    0608

--- 0608/103. Linked List Cycle II.py
+++ 0608/103. Linked List Cycle II.py
@@ -1,6 +1,4 @@
-
 class ListNode(object):
-
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
@@ -11,6 +9,7 @@ class Solution:
     @param: head: The first node of linked list.
     @return: The node where the cycle begins. if there is no cycle, return null
     """
+
     def detectCycle(self, head):
         if head is None:
             return False
@@ -30,17 +29,12 @@ class Solution:
         return False
 
 
-
-
-
-
 if __name__ == "__main__":
-    #21->10->4->5
+    # 21->10->4->5
 
     root = ListNode(21)
     root.next = ListNode(10)
     root.next.next = ListNode(4)
     root.next.next.next = ListNode(5)
     root.next.next.next.next = root
-
-
+    print Solution().detectCycle(root).val
