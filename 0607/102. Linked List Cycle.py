8ac4273929b10a627cc13086bd9239bba066be93
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-07 20:15:52 -0400

    0607

---
+++ 0607/102. Linked List Cycle.py
@@ -0,0 +1,30 @@
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param head: The first node of the linked list.
+    @return: True if it has a cycle, or false
+    """
+    def hasCycle(self, head):
+        # write your code here
+        if head is None:
+            return False
+        fast = head
+        slow = head
+        while True:
+            if fast.next is not None:
+                fast = fast.next.next
+                slow = slow.next
+                if fast is None:
+                    return False
+                elif fast == slow:
+                    return True
+            else:
+                return False
+        return False
--- 0607/380. Intersection of Two Linked Lists.py
+++ 0607/380. Intersection of Two Linked Lists.py
@@ -4,39 +4,33 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
     # @param headA: the first list
     # @param headB: the second list
     # @return: a ListNode
     def getIntersectionNode(self, headA, headB):
         # Write your code here
-        listA = []
-        listB = []
-        if headA == None or headB == None:
-            return None
-        while 1:
-            if headA == None:
-                break;
-            listA.append(headA.val)
-            headA = headA.next
+        lenA, lenB = 0, 0
+        node1, node2 = headA, headB
+        while node1 is not None:
+            lenA += 1
+            node1 = node1.next
+        while node2 is not None:
+            lenB += 1
+            node2 = node2.next
 
-        while 1:
-            if headB == None:
-                break;
-            listB.append(headB.val)
-            headB = headB.next
+        node1, node2 = headA, headB
+        while lenA > lenB:
+            node1 = node1.next
+            lenA -= 1
+        while lenB > lenA:
+            node2 = node2.next
+            lenB -= 1
+        while node1 is not node2:
+            node1 = node1.next
+            node2 = node2.next
+        return node1
 
-        if listA[-1] != listB[-1]:
-            return None;
 
-        if len(listA) < len(listB):
-            minLen = len(listA)
-        else:
-            minLen = len(listB)
-
-        inster = []
-        for i in range(1, minLen + 1):
-            if listA[-i] != listB[-i]:
-                return ListNode(listA[-i + 1])
-            if i == minLen:
-                return ListNode(listA[-i])
+print Solution().getIntersectionNode(None, None)
