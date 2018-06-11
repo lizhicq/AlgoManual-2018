554a2bc70183910264e23155afe342303d26e0fd
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-10 20:49:21 -0400

    0609

--- 0610-BFS/615. Course Schedule.py
+++ 0610-BFS/615. Course Schedule.py
@@ -3,7 +3,7 @@ class Solution:
     # @param {int[][]} prerequisites a list of prerequisite pairs
     # @return {boolean} true if can finish all courses or false
     def canFinish(self, numCourses, prerequisites):
-
+        from Queue import deque
         edges = {i: [] for i in range(numCourses)}
         degrees = [0 for i in range(numCourses)]
 
@@ -11,21 +11,26 @@ class Solution:
         for i, j in prerequisites:
             edges[j].append(i)
             degrees[i] += 1
-        queue, count = [], 0
+        queue, count = deque(), 0
 
         # 2. init queue
         for i in range(numCourses):
             if degrees[i] == 0:
-                queue.append(i)
+                queue.appendleft(i)
 
         # 3. bfs
         while len(queue) > 0:
-            node = queue.pop(0)
+            node = queue.pop()
             count += 1
 
             for x in edges[node]:
                 degrees[x] -= 1
                 if degrees[x] == 0:
-                    queue.append(x)
+                    queue.appendleft(x)
 
         return count == numCourses
+
+if __name__ == "__main__":
+    numCourses = 2
+    prerequisites = [[1,0]]
+    print Solution().canFinish(numCourses, prerequisites)
