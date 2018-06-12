84665b2f81fd2996323ed538fd4db65f2ccad7f9
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-12 19:42:16 -0400

    0612

---
+++ 0612/543. Kth Largest in N Arrays.py
@@ -0,0 +1,8 @@
+class Solution:
+    """
+    @param arrays: a list of array
+    @param k: An integer
+    @return: an integer, K-th largest element in N arrays
+    """
+    def KthInArrays(self, arrays, k):
+        # write your code here
