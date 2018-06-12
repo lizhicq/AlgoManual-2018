3fa141ad2db9e5c431e9e7cacd0f0635cbc7e304
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-11 21:06:44 -0400

    0611

--- 0611/386. Longest Substring with At Most K Distinct Characters.py
+++ 0611/386. Longest Substring with At Most K Distinct Characters.py
@@ -6,18 +6,27 @@ class Solution:
     """
     def lengthOfLongestSubstringKDistinct(self, s, k):
         j = 0
-        ch_set = set()
+        ch_dic = {}
         ans = 0
         for i in range(len(s)):
-            while j < len(s) and len(ch_set) <= k:
-                ch_set.add(s[j])
+            while j < len(s):
+                if s[j] in ch_dic:
+                    ch_dic[s[j]] += 1
+                else:
+                    if len(ch_dic) == k:
+                        break
+                    ch_dic[s[j]] = 1
                 j += 1
-            if len(ch_set) <= k:
-                ans = max(ans, j-i)
-            ch_set.remove(s[i])
+
+            ans = max(ans, j-i)
+            if s[i] in ch_dic:
+                if ch_dic[s[i]] > 1:
+                    ch_dic[s[i]] -= 1
+                else:
+                    ch_dic.pop(s[i])
         return ans
 
 if __name__ =="__main__":
-    s = "eceba"
-    k = 3
+    s = "eqgkcwGFvjjmxutystqdfhuMblWbylgjxsxgnoh"
+    k = 16
     print Solution().lengthOfLongestSubstringKDistinct(s, k)
