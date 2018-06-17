658c400c7894d4ab9827a63efe4db8fcd93c7137
Zhi Li <lizhicq@users.noreply.github.com>
2018-06-16 22:51:08 -0400

    0616

--- 0616/120. Word Ladder.py
+++ 0616/120. Word Ladder.py
@@ -10,10 +10,30 @@ class Solution:
         from sys import maxsize
         deck = deque()
         deck.append(start)
-        record = {word}
+        record = {word:maxsize for word in dict}
+        record[start] = 1
+        record[end] = maxsize
+        def check(word, new_word):
+            count = 0
+            for i in range(len(word)):
+                if word[i] != new_word[i]:
+                    count += 1
+                if count > 1:
+                    break
+            return count == 1
+
         while len(deck) > 0:
             word = deck.popleft()
-            for new_word in dict:
-                if len(set(word) - set(new_word)) == 1:
-                    if record[new_word] < record[word] + 1:
+            for new_word in record:
+                if check(word, new_word):
+                    if record[new_word] > record[word] + 1:
+                        record[new_word] = record[word] + 1
+                        deck.append(new_word)
+        print record
+        return record[end]
 
+if __name__ == "__main__":
+    start = 'kiss'
+    end = 'tusk'
+    dict = ["miss","dusk","kiss","musk","tusk","diss","disk","sang","ties","muss"]
+    print Solution().ladderLength(start, end, dict)
